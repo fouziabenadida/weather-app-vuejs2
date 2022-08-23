ad72f37e8a431f9fa91acef0bd6da452ca390b84
fouziabenadida <benadiafouzia@gmail.com>
2022-08-23 21:47:59 +0300

    refactor

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
